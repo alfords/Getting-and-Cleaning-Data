2e4cfd7ea68d906ddec34b9acfda3a4abb043ad0
alfords <aaron_zhang@ymail.com>
2014-04-14 15:20:11 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,37 +1,85 @@
 
-# Read feature names and activity names
-features <- read.table("features.txt")
-features_name <- features[, 2]
-activity_labels <- read.table("activity_labels.txt")
-activity_name <- activity_labels[, 2]
 
-# Read train, test values and labels
-train_value <- read.table("train/X_train.txt")
-train_label <- read.table("train/y_train.txt")
-test_value <- read.table("test/X_test.txt")
-test_label <- read.table("test/y_test.txt")
+dtSubjectTrain <- fread(file.path(pathIn, "train", "subject_train.txt"))
+dtSubjectTest <- fread(file.path(pathIn, "test", "subject_test.txt"))
+ 
+dtActivityTrain <- fread(file.path(pathIn, "train", "Y_train.txt"))
+dtActivityTest <- fread(file.path(pathIn, "test", "Y_test.txt"))
 
-# Uses descriptive activity names to name the activities in the data set
-train_label <- factor(train_label[,1], labels=activity_name)
-test_label <- factor(test_label[,1], labels=activity_name)
+fileToDataTable <- function(f) {
+       df <- read.table(f)
+       dt <- data.table(df)
+}
+dtTrain <- fileToDataTable(file.path(pathIn, "train", "X_train.txt"))
+dtTest <- fileToDataTable(file.path(pathIn, "test", "X_test.txt"))
 
-# Make names of value to be feature names
-names(train_value) <- features_name
-names(test_value) <- features_name
+dtSubject <- rbind(dtSubjectTrain, dtSubjectTest)
+setnames(dtSubject, "V1", "subject")
+dtActivity <- rbind(dtActivityTrain, dtActivityTest)
+setnames(dtActivity, "V1", "activityNum")
+dt <- rbind(dtTrain, dtTest)
 
-# Choose the features on the mean and standard deviation for each measurement.
-target <- grep("mean\\(\\)|std\\(\\)", features_name)
+dtSubject <- cbind(dtSubject, dtActivity)
+dt <- cbind(dtSubject, dt)
 
-# Bind values and labels, also extracts the mean and std data
-train_data <- cbind(train_label, train_value[, target])
-test_data <- cbind(test_label, test_value[, target])
+setkey(dt, subject, activityNum)
 
-# Make name of label data to be "activity"
-names(train_data)[1] <- "activity"
-names(test_data)[1] <- "activity"
+dtFeatures <- fread(file.path(pathIn, "features.txt"))
+setnames(dtFeatures, names(dtFeatures), c("featureNum", "featureName"))
 
-# Bind train data and test data
-clean_data <- rbind(train_data, test_data)
+dtFeatures <- dtFeatures[grepl("mean\\(\\)|std\\(\\)", featureName)]
 
-# Create new clean data into "clean_data.txt"
-write.table(clean_data, "clean_data.txt")
+dtFeatures$featureCode <- dtFeatures[, paste0("V", featureNum)]
+head(dtFeatures)
+
+dtFeatures$featureCode
+
+select <- c(key(dt), dtFeatures$featureCode)
+dt <- dt[, select, with = FALSE]
+
+dtActivityNames <- fread(file.path(pathIn, "activity_labels.txt"))
+setnames(dtActivityNames, names(dtActivityNames), c("activityNum", "activityName"))
+
+dt <- merge(dt, dtActivityNames, by = "activityNum", all.x = TRUE)
+setkey(dt, subject, activityNum, activityName)
+dt <- data.table(melt(dt, key(dt), variable.name = "featureCode"))
+dt <- merge(dt, dtFeatures[, list(featureNum, featureCode, featureName)], by = "featureCode", 
+            all.x = TRUE)
+
+dt$activity <- factor(dt$activityName)
+dt$feature <- factor(dt$featureName)
+
+grepthis <- function(regex) {
+  grepl(regex, dt$feature)
+}
+## Features with 2 categories
+n <- 2
+y <- matrix(seq(1, n), nrow = n)
+x <- matrix(c(grepthis("^t"), grepthis("^f")), ncol = nrow(y))
+dt$featDomain <- factor(x %*% y, labels = c("Time", "Freq"))
+x <- matrix(c(grepthis("Acc"), grepthis("Gyro")), ncol = nrow(y))
+dt$featInstrument <- factor(x %*% y, labels = c("Accelerometer", "Gyroscope"))
+x <- matrix(c(grepthis("BodyAcc"), grepthis("GravityAcc")), ncol = nrow(y))
+dt$featAcceleration <- factor(x %*% y, labels = c(NA, "Body", "Gravity"))
+x <- matrix(c(grepthis("mean()"), grepthis("std()")), ncol = nrow(y))
+dt$featVariable <- factor(x %*% y, labels = c("Mean", "SD"))
+## Features with 1 category
+dt$featJerk <- factor(grepthis("Jerk"), labels = c(NA, "Jerk"))
+dt$featMagnitude <- factor(grepthis("Mag"), labels = c(NA, "Magnitude"))
+## Features with 3 categories
+n <- 3
+y <- matrix(seq(1, n), nrow = n)
+x <- matrix(c(grepthis("-X"), grepthis("-Y"), grepthis("-Z")), ncol = nrow(y))
+dt$featAxis <- factor(x %*% y, labels = c(NA, "X", "Y", "Z"))
+
+r1 <- nrow(dt[, .N, by = c("feature")])
+r2 <- nrow(dt[, .N, by = c("featDomain", "featAcceleration", "featInstrument", 
+                           "featJerk", "featMagnitude", "featVariable", "featAxis")])
+r1 == r2
+
+setkey(dt, subject, activity, featDomain, featAcceleration, featInstrument, 
+       featJerk, featMagnitude, featVariable, featAxis)
+dtTidy <- dt[, list(count = .N, average = mean(value)), by = key(dt)]
+
+f <- file.path(path, "project.RData")
+save(dt, dtTidy, file = f)
